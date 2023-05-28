823890934d3d8f5e9582fc295eb3ecbe0ed38dc7
malikfaraz63 <malikfaraz63@gmail.com>
2023-05-28 13:09:55 +0100

    1.B Added structs for CPU state and began data processing (immediate)

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,50 @@
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char **argv) {
   return EXIT_SUCCESS;
 }
+
+// MARK: Registers and state
+
+struct PSTATE {
+    bool negativeFlag;
+    bool zeroFlag;
+    bool carryFlag;
+    bool overflowFlag;
+};
+
+struct Registers {
+    // SPECIAL
+    long long zeroRegister;
+    long long programCounter;
+    struct PSTATE stateRegister;
+
+    // GENERAL
+    char registers[8 * 31];
+};
+
+// MARK: Data Processing Instruction (immediate)
+
+enum DpOpcArithmetic {
+    ADD,
+    ADDS,
+    SUB,
+    SUBS
+};
+
+enum DpOpcWideMove {
+    MOVZ,
+    INVALID,
+    MOVN,
+    MOVK
+};
+
+
+
+enum ShiftOP {
+    LSL,
+    LSR,
+    ASR,
+    ROR
+};
