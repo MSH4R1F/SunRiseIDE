4ee27b15b9ab159de402871931f07eae079c2361
Mohamed Sharif <mas222@ic.ac.uk>
2023-05-30 15:50:29 +0100

    Implemented rotateRight and decreased duplication between code

--- src/emulator/bitwiseshift.c
+++ src/emulator/bitwiseshift.c
@@ -21,17 +21,16 @@ uint32_t logicalRightShift(uint32_t number, uint32_t shift) {
 
 
 
-uint32_t arithmeticLeftShift(uint32_t number, uint32_t shift) {
-    if (shift > WORDSIZE) {
-        return 0;
-    }
-    return (int32_t) (number << shift);
-}
-
 
 uint32_t arithmeticRightShift(uint32_t number, uint32_t shift) {
     if (shift > WORDSIZE) {
         return 0;
     }
     return (int32_t) (number << shift);
+}
+uint32_t rotateRight(uint32_t number, uint32_t shift) {
+    if (shift > WORDSIZE) {
+        return 0;
+    }
+    return (number >> shift) | (number << (32 - shift));
 }
--- src/emulator/bitwiseshift.h
+++ src/emulator/bitwiseshift.h
@@ -7,7 +7,7 @@
 #include <stdint.h>
 uint32_t logicalLeftShift(uint32_t number, uint32_t shift);
 uint32_t logicalRightShift(uint32_t number, uint32_t shift);
-uint32_t arithmeticLeftShift(uint32_t number, uint32_t shift);
-uint32_t arithmeticLeftShift(uint32_t number, uint32_t shift);
+uint32_t arithmeticRightShift(uint32_t number, uint32_t shift);
+uint32_t rotateRight(uint32_t number, uint32_t shift);
 
 #endif //ARMV8_27_BITWISESHIFT_H
