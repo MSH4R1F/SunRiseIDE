b95a2394d1c8bca2a7dc52071ff1a8cd88aba0c2
malikfaraz63 <malikfaraz63@gmail.com>
2023-05-30 12:53:56 +0100

    1.C.2.1 Created branch.

--- src/emulator/dataProcessingImm.c
+++ src/emulator/dataProcessingImm.c
@@ -2,3 +2,8 @@
 // Created by Faraz Malik on 29/05/2023.
 //
 #include "dataProcessingImm.h"
+#include <stdbool.h>
+
+bool isDataProcessingImm(long long op0) {
+    return true;
+}
--- src/emulator/dataProcessingReg.c
+++ src/emulator/dataProcessingReg.c
@@ -3,3 +3,5 @@
 //
 
 #include "dataProcessingReg.h"
+
+bool
