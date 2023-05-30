c8bbc275ee73c7d374ad29d3dc8fc743b4707ba8
Kareem Mehanna <km1422@ic.ac.uk>
2023-05-30 15:58:42 +0100

    1.C.2.2_dp_reg - completed executeLogic function and created new branch

--- src/emulator/dataProcessingReg.h
+++ src/emulator/dataProcessingReg.h
@@ -5,15 +5,12 @@
 #ifndef ARMV8_27_DATAPROCESSINGREG_H
 #define ARMV8_27_DATAPROCESSINGREG_H
 
-bool isDataProcessingReg(long long op0);
+bool isDataProcessingReg(uint_32_t op0);
 
-void executeDataProcessingReg(long long instruction, struct Registers registers);
+void executeDataProcessingReg(uint_32_t instruction, struct Registers *registers);
 
-bool isArithmetic(long long opr);
-bool isLogic(long long opr);
-
-void executeArithmeticProcessingReg(long long instruction, struct Registers registers);
-void executeLogicProcessingReg(long long instruction, struct Registers registers);
-void executeMultiplyProcessingReg(long long instruction, struct Registers registers);
+void executeArithmeticProcessingReg(uint_32_t instruction, struct Registers *registers);
+void executeLogicProcessingReg(uint_32_t instruction, struct Registers *registers);
+void executeMultiplyProcessingReg(uint_32_t instruction, struct Registers *registers);
 
 #endif //ARMV8_27_DATAPROCESSINGREG_H
