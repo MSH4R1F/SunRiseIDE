99f0ad0825890841fcef211fc58ae99c51f5219e
Mohamed Sharif <mas222@ic.ac.uk>
2023-05-30 15:35:21 +0100

    1.C.2.3: Completed the bitewises shift functions to be used in dataProcessing

---
+++ src/emulator/bitwiseshift.c
@@ -0,0 +1,37 @@
+//
+// Created by mshar1f on 30/05/23.
+//
+#include "bitwiseshift.h"
+#define WORDSIZE  32
+
+uint32_t logicalLeftShift(uint32_t number, uint32_t shift) {
+    if (shift > WORDSIZE) {
+        return 0;
+    }
+    return number << shift;
+}
+
+
+uint32_t logicalRightShift(uint32_t number, uint32_t shift) {
+    if (shift > WORDSIZE) {
+        return 0;
+    }
+    return number >> shift;
+}
+
+
+
+uint32_t arithmeticLeftShift(uint32_t number, uint32_t shift) {
+    if (shift > WORDSIZE) {
+        return 0;
+    }
+    return (int32_t) (number << shift);
+}
+
+
+uint32_t arithmeticRightShift(uint32_t number, uint32_t shift) {
+    if (shift > WORDSIZE) {
+        return 0;
+    }
+    return (int32_t) (number << shift);
+}
---
+++ src/emulator/bitwiseshift.h
@@ -0,0 +1,13 @@
+//
+// Created by mshar1f on 30/05/23.
+//
+
+#ifndef ARMV8_27_BITWISESHIFT_H
+#define ARMV8_27_BITWISESHIFT_H
+#include <stdint.h>
+uint32_t logicalLeftShift(uint32_t number, uint32_t shift);
+uint32_t logicalRightShift(uint32_t number, uint32_t shift);
+uint32_t arithmeticLeftShift(uint32_t number, uint32_t shift);
+uint32_t arithmeticLeftShift(uint32_t number, uint32_t shift);
+
+#endif //ARMV8_27_BITWISESHIFT_H
