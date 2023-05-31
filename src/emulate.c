ff6961110c831151985cbd2394609bd4ec1070f2
malikfaraz63 <malikfaraz63@gmail.com>
2023-05-31 12:52:46 +0100

    1.C.2 Combined into emulate.c due to circumvent file dependency issues.

---
+++ src/emulate.c
@@ -0,0 +1,397 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+// FILE: utils.h
+
+uint32_t fetchInstruction(long long programCounter, char *filename);
+
+// FILE: registers.h
+
+struct PSTATE;
+struct Registers;
+
+// FILE: bitwiseshift.h
+
+long long logicalLeftShift(long long number, uint32_t shift);
+long long logicalRightShift(long long number, uint32_t shift);
+long long arithmeticRightShift(long long number, uint32_t shift);
+long long rotateRight(long long number, uint32_t shift);
+
+// FILE: dataProcessingImm.h
+
+bool isDataProcessingImm(long long op0);
+void executeDataProcessingImm(long long instruction, struct Registers *registers);
+
+bool isArithmeticProcessing(long long opi);
+
+void executeArithmeticProcessingImm(long long instruction, struct Registers *registers);
+void executeWideMoveProcessing(long long instruction, struct Registers *registers);
+
+// FILE: dataProcessingReg.h
+
+bool isDataProcessingReg(uint32_t op0);
+
+void executeDataProcessingReg(uint32_t instruction, struct Registers *registers);
+
+void executeArithmeticProcessingReg(uint32_t instruction, struct Registers *registers);
+void executeLogicProcessingReg(uint32_t instruction, struct Registers *registers);
+void executeMultiplyProcessingReg(uint32_t instruction, struct Registers *registers);
+
+// FILE: singleDataTransfer.h
+
+void executeDataTransfer(long long instruction, struct Registers *registers);
+
+// FILE: branch.h
+
+bool isBranch(long long op0);
+void executeBranch(long long instruction, struct Registers *registers);
+
+// FILE: utils.c
+
+uint32_t fetchInstruction(long long programCounter, char *filename) {
+    FILE *file = fopen(filename, "rb");  // Open the binary file in read mode
+
+    if (file == NULL) {
+        printf("Failed to open the file.\n");
+        return 1;
+    }
+
+    uint32_t value;  // Variable to store the read integer
+
+    if (fseek(file, programCounter, SEEK_SET) != 0) {
+        printf("Failed to seek to the desired position.\n");
+        fclose(file);
+        return 1;
+    }
+
+    // Read 4 bytes from the file into the variable 'value'
+    if (fread(&value, sizeof(value), 1, file) != 1) {
+        printf("Failed to read from the file.\n");
+        fclose(file);
+        return 1;
+    }
+
+    // Convert the value from little-endian to the system's endianness if necessary
+    value = ntohl(value);  // Use ntohl() for network-to-host conversion
+
+    fclose(file);  // Close the file
+
+    return value;
+}
+
+// FILE: registers.c
+
+struct PSTATE {
+    bool negativeFlag;
+    bool zeroFlag;
+    bool carryFlag;
+    bool overflowFlag;
+};
+
+struct Registers {
+    // SPECIAL
+    long long zeroRegister;
+    long long programCounter;
+    struct PSTATE stateRegister;
+
+    // GENERAL
+    long long registers[31];
+};
+
+// FILE: bitwiseshift.c
+
+#define WORDSIZE  32
+
+long long logicalLeftShift(long long number, uint32_t shift) {
+    if (shift > WORDSIZE) {
+        return 0;
+    }
+    return number << shift;
+}
+
+long long logicalRightShift(long long number, uint32_t shift) {
+    if (shift > WORDSIZE) {
+        return 0;
+    }
+    return number >> shift;
+}
+
+long long arithmeticRightShift(long long number, uint32_t shift) {
+    if (shift > WORDSIZE) {
+        return 0;
+    }
+    return (int32_t) (number << shift);
+}
+
+long long rotateRight(long long number, uint32_t shift) {
+    if (shift > WORDSIZE) {
+        return 0;
+    }
+    return (number >> shift) | (number << (32 - shift));
+}
+
+// FILE: dataProcessingImm.c
+
+enum DpOpcArithmetic {
+    ADD,
+    ADDS,
+    SUB,
+    SUBS
+};
+
+enum DpOpcWideMove {
+    MOVZ,
+    INVALID,
+    MOVN,
+    MOVK
+};
+
+bool isDataProcessingImm(long long op0) {
+    long long match = 0x9;
+    return (op0 | 0x1) == match;
+}
+void executeDataProcessingImm(long long instruction, struct Registers *registers) {
+    long long opi = (instruction >> 23) & 0x7;
+
+    if (isArithmeticProcessing(opi)) {
+        executeArithmeticProcessingImm(instruction, registers);
+    } else {
+        executeWideMoveProcessing(instruction, registers);
+    }
+}
+
+bool isArithmeticProcessing(long long opi) {
+    long long match = 0x2;
+    return opi == match;
+}
+void executeArithmeticProcessingImm(long long instruction, struct Registers *registers) {
+    uint32_t rd = instruction & 0x1F;
+    uint32_t rn = (instruction >> 5) & 0x1F;
+
+    uint32_t op = (instruction >> 10) & 0xFFF;
+    uint32_t sh = (instruction >> 22) & 0x1;
+
+    if (sh == 1) {
+        op = op << 12;
+    }
+
+    enum DpOpcArithmetic opc = (instruction >> 29) & 0x3;
+
+    bool setsFlags = opc == ADDS || opc == SUBS;
+    if (rn == 31 && setsFlags) {
+        return;
+    }
+    long long reg = registers->zeroRegister;
+    if (rn < 31) {
+        reg = registers->registers[rn];
+    }
+
+    long long res;
+    if (opc == ADD || opc == SUB) {
+        res = reg + op;
+    } else {
+        res = reg - op;
+    }
+
+    // Complete the flags
+    if (setsFlags) {
+        registers->stateRegister.negativeFlag = res < 0;
+        registers->stateRegister.zeroFlag = res == 0;
+    }
+
+    if (rd < 31) {
+        registers->registers[rd] = res;
+    }
+}
+
+void executeWideMoveProcessing(long long instruction, struct Registers *registers) {
+    uint32_t rd = instruction & 0x1F;
+    uint32_t hw = (instruction >> 21) & 0x3;
+    uint64_t imm16 = (instruction >> 5) & 0xFFFF;
+
+    uint32_t shift = hw * 16;
+    uint64_t op = imm16 << shift;
+
+    uint32_t sf = (instruction >> 31);
+
+    enum DpOpcWideMove opc = (instruction >> 29) & 0x3;
+
+    long long res = registers->registers[rd];
+    if (opc == MOVZ) {
+        res = op;
+    } else if (opc == MOVN) {
+        res = ~op;
+    } else {
+        res = res & ~(0xFFFF << shift);
+        res = res | (imm16 << shift);
+    }
+
+    if (sf == 0) {
+        res = res & 0xFFFFFFFF;
+    }
+
+    if (rd < 31) {
+        registers->registers[rd] = res;
+    }
+}
+
+// FILE: dataProcessingReg.c
+
+long long shiftFun(uint32_t shift, long long reg, uint32_t operand) {
+    if (shift == 0) {
+        return logicalLeftShift(reg, operand);
+    } else if (shift == 1) {
+        return logicalRightShift(reg, operand) ;
+    } else if (shift == 2) {
+        return arithmeticRightShift(reg, operand);
+    } else {
+        return rotateRight(reg, shift);
+    }
+}
+
+bool overunderflow(long long val1, long long val2, long long result) {
+    bool sign1 = val1 < 0;
+    bool sign2 = val2 < 0;
+    bool signResult = result < 0;
+    if (sign1 == sign2 && sign1 != signResult) {
+        return true;
+    }
+    return false;
+}
+
+bool carry(bool isPlus, bool overunderflow) {
+    if (isPlus) {
+        if (overunderflow) {
+            return 1;
+        }
+        return 0;
+    } else {
+        if (overunderflow) {
+            return 0;
+        }
+        return 1;
+    }
+}
+
+void executeDataProcessingReg(uint32_t instruction, struct Registers *registers) {
+    uint32_t opr = (instruction >> 21) & 0x1F;
+    uint32_t first2 = (opr >> 3);
+    if (first2 == 1) {
+        executeArithmeticProcessingReg(instruction, registers);
+    } else if (first2 == 0) {
+        executeLogicProcessingReg(instruction, registers);
+    } else {
+        executeMultiplyProcessingReg(instruction, registers);
+    }
+}
+
+void executeArithmeticProcessingReg(uint32_t instruction, struct Registers *registers) {
+    uint32_t shift = (instruction >> 22) & 0x3;
+    uint32_t opr = (instruction >> 10) & 0x1F;
+    uint32_t rn = (instruction >> 4) & 0x1F;
+    long long rn_val = registers->registers[rn];
+    uint32_t rm = (instruction >> 16) & 0x1F;
+    long long rm_val = registers->registers[rm];
+    uint32_t opc = (instruction >> 28) & 0x1;
+    uint32_t rd = instruction & 0x1F;
+    long long rd_val;
+    long long op2 = shiftFun(shift, rm_val, opr);
+    int multiplier = -1;
+    if (opc / 2 == 0) {
+        multiplier *= -1;
+    }
+    rd_val = rn_val + multiplier*op2;
+    if (opc % 2 == 1) {
+        bool V = overunderflow(rn_val, multiplier*op2, rd_val);
+        registers->stateRegister.negativeFlag = rd_val < 0;
+        registers->stateRegister.zeroFlag = rd_val == 0;
+        registers->stateRegister.carryFlag = carry(multiplier == 1, V);
+        registers->stateRegister.overflowFlag = V;
+    }
+    registers->registers[rd] = rd_val;
+}
+
+void executeLogicProcessingReg(uint32_t instruction, struct Registers *registers) {
+    uint32_t shift = (instruction >> 22) & 0x3;
+    uint32_t N = (instruction >> 21) & 0x1;
+    uint32_t opc = (instruction >> 29) & 0x3;
+    uint32_t operand = (instruction >> 10) & 0x1F;
+    uint32_t rn = (instruction >> 5) & 0x1F;
+    long long rn_val = registers->registers[rn];
+    uint32_t rm = (instruction >> 16) & 0x1F;
+    long long rm_val = registers->registers[rm];
+    rm_val = shiftFun(shift, rm_val, operand);
+    uint32_t combined_opc = (opc << 1) + N;
+
+    uint32_t rd = instruction & 0x1F;
+    if (rd < 30) {
+        int64_t rd_val = 0;
+        switch (combined_opc) {
+            case 0:
+                rd_val = rn_val & rm_val;
+                break;
+            case 1:
+                rd_val = rn_val & ~rm_val;
+                break;
+            case 2:
+                rd_val = rn_val | rm_val;
+                break;
+            case 3:
+                rd_val = rn_val | ~rm_val;
+                break;
+            case 4:
+                rd_val = rn_val ^ ~rm_val;
+                break;
+            case 5:
+                rd_val = rn_val ^ rm_val;
+                break;
+            case 6:
+                rd_val = rn_val & rm_val;
+                registers->stateRegister.negativeFlag = rd_val < 0;
+                registers->stateRegister.zeroFlag = rd_val == 0;
+                registers->stateRegister.carryFlag = false;
+                registers->stateRegister.overflowFlag = false;
+                break;
+            case 7:
+                rd_val = rn_val & ~rm_val;
+                registers->stateRegister.negativeFlag = rd_val < 0;
+                registers->stateRegister.zeroFlag = rd_val == 0;
+                registers->stateRegister.carryFlag = false;
+                registers->stateRegister.overflowFlag = false;
+                break;
+        }
+        registers->registers[rd] = rd_val;
+    }
+}
+
+void executeMultiplyProcessingReg(uint32_t instruction, struct Registers *registers) {}
+
+// MARK: Fetch-Decode-Execute cycle
+
+void processor() {
+    struct PSTATE stateRegister = { false, false, false, false };
+
+    struct Registers registers;
+    registers.programCounter = 0;
+    registers.zeroRegister = 0;
+    registers.stateRegister = stateRegister;
+
+    uint32_t instruction = fetchInstruction(registers.programCounter, "add01_exp.bin");
+    long long op0 = (instruction >> 25) & 0xF;
+
+    if (isDataProcessingImm(op0)) {
+        executeDataProcessingImm(instruction, &registers);
+    } else if (isDataProcessingReg(op0)) {
+        executeDataProcessingReg(instruction, &registers);
+    } else if (isBranch(op0)) {
+        executeBranch(instruction, &registers);
+    } else {
+        executeDataTransfer(instruction, &registers);
+    }
+}
+
+int main(int argc, char **argv) {
+    processor();
+    return EXIT_SUCCESS;
+}
--- src/emulator/branch.h
+++ src/emulator/branch.h
@@ -5,7 +5,4 @@
 #ifndef ARMV8_27_BRANCH_H
 #define ARMV8_27_BRANCH_H
 
-bool isBranch(long long op0);
-void executeBranch(long long instruction, struct Registers registers);
-
 #endif //ARMV8_27_BRANCH_H
