72fb2eadaf21105d68734c9a7fdb73c14e5b5186
Kareem Mehanna <km1422@ic.ac.uk>
2023-06-01 20:35:48 +0100

    Added test files

--- src/assemble.c
+++ src/assemble.c
@@ -3,9 +3,11 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    long long num = 3;
-    uint32_t hello = num;
-    printf("%u\n", hello);
+    long long word = 0xFFFFFFFF;
+    if (word & 0x80000000) {
+        word = word | (0xFFFFFFFF << 32);
+    }
+    printf("%lld\n", word);
     return EXIT_SUCCESS;
 }
 
