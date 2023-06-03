17dab0f6af6274ac330df7e9e22aceea5fbca0d0
malikfaraz63 <malikfaraz63@gmail.com>
2023-06-03 19:47:54 +0100

    1.C.3.2 File purge for refactoring.

--- src/emulator/branch.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Faraz Malik on 29/05/2023.
-//
-
-#ifndef ARMV8_27_BRANCH_H
-#define ARMV8_27_BRANCH_H
-#include "registers.h"
-void executeBranch(long long instruction, struct Registers *registers);
-#endif //ARMV8_27_BRANCH_H
--- src/emulator/singleDataTransfer.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Faraz Malik on 29/05/2023.
-//
-
-#include "singleDataTransfer.h"
