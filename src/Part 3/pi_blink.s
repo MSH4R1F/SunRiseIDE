d6d830d5a412393e8da54d8a361729664e735219
Kareem Mehanna <km1422@ic.ac.uk>
2023-06-19 14:39:08 +0100

    3 - created and tested assembly file and img file

--- src/Part 3/pi_blink.s
+++ src/Part 3/pi_blink.s
@@ -5,14 +5,14 @@ movz w2, #0x8000, lsl #16
 movz w6, #0x4000, lsl #16
 movz w5, #0x0008, lsl #16
 
-movz w3, #0x0ac0, lsl #16
-
-wait_write:
+on_wait_write:
     ldr w1, [w0, #0x38]
     tst w1, w2
-    b.ne wait_write
+    b.ne on_wait_write
 
-write_buffer:
+on_write_buffer:
+    movz w3, #0x0ac0, lsl #16
+
     movz w4, #0x20
     str w4, [w3, #0]
 
@@ -29,7 +29,7 @@ write_buffer:
     movz w4, #0x0
     str w4, [w3, #16]
 
-    movz w4, #0x82
+    movz w4, #130
     str w4, [w3, #20]
 
     movz w4, #0x1
@@ -42,3 +42,68 @@ write_buffer:
 
     str w3, [w0, #0x20]
 
+on_wait_read:
+    ldr w1, [w0, #0x18]
+    tst w1, w6
+    b.ne on_wait_read
+
+on_read_response:
+    ldr w1, [w0, #0]
+
+movz w29, #0x069, lsl #16
+on_light_delay:
+    subs w29, w29, #1
+    cmp w29, #0
+    b.ne on_light_delay
+
+off_wait_write:
+    ldr w1, [w0, #0x38]
+    tst w1, w2
+    b.ne off_wait_write
+
+off_write_buffer:
+    movz w3, #0x0ac0, lsl #16
+
+    movz w4, #0x20
+    str w4, [w3, #0]
+
+    movz w4, #0x0
+    str w4, [w3, #4]
+
+    movz w4, #0x0003, lsl #16
+    movk w4, #0x8041
+    str w4, [w3, #8]
+
+    movz w4, #0x8
+    str w4, [w3, #12]
+
+    movz w4, #0x0
+    str w4, [w3, #16]
+
+    movz w4, #130
+    str w4, [w3, #20]
+
+    movz w4, #0x0
+    str w4, [w3, #24]
+
+    movz w4, #0x0
+    str w4, [w3, #28]
+
+    add w3, w3, #8
+
+    str w3, [w0, #0x20]
+
+off_wait_read:
+    ldr w1, [w0, #0x18]
+    tst w1, w6
+    b.ne off_wait_read
+
+ldr w1, [w0, #0]
+
+movz w29, #0x0069, lsl #16
+off_light_delay:
+    subs w29, w29, #1
+    cmp w29, #0
+    b.ne off_light_delay
+
+b on_wait_write
